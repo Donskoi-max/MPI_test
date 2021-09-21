ea82ff8c24b3fff1ae57b1997a5643d00452e93c
“Donskoi-max” <191870294@smail.nju.edu.cn>
2021-09-21 11:05:19 +0800

    commit trial

---
+++ hellompi.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstring>
+#include<mpi.h>
+using namespace std;
+int main(int argc,char**argv){
+        int myid,numprocs,source;
+        MPI_Status status;
+        char message[100];
+        MPI_Init(&argc,&argv);
+        MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+        MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+        if(myid!=0){
+                strcpy(message,"Hello World!");
+                MPI_Send(message,strlen(message)+1,MPI_CHAR,0,99,MPI_COMM_WORLD);
+        }
+        else{
+                for(source=1;source<numprocs;source++){
+                        MPI_Recv(message,100,MPI_CHAR,source,99,MPI_COMM_WORLD,&status);
+                        cout<<"I am process"<<myid<<". I recv string '"<<message<<"' from process "<<source<<endl;
+                }
+        }
+        MPI_Finalize();
+}
